b258ece0b6eb126e5d3e30d2da9a72fd59318490
Paulo Sergio Romao Junior <PauloSergioRomaoJunior@users.noreply.github.com>
2017-01-08 15:54:35 -0200

    Add files via upload

--- src/conversor.js
+++ src/conversor.js
@@ -1,9 +1,29 @@
 function converter(data, callback){
 	
 	var reader = new window.FileReader();
- 		reader.readAsDataURL(data);
+ 		reader.readAsDataURL(data[0]);
  		reader.onloadend = function(){
- 				callback(reader.result);
+ 				callback(reader.result,{nome: data[1],type: data[2]});
  		};
  		
+}
+function salvar(data, callback){
+
+				var [nome, ext, uri, save] = data;
+
+				var http = new XMLHttpRequest(),
+					url  = 'create.php',
+					param = 'nome='+nome+'&extensao='+ext+'&arq='+uri+'&save='+save;
+
+				http.open("POST", url, true);
+				http.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+
+				http.onreadystatechange = function() {
+					if(http.readyState == 4 && http.status == 200) {
+						callback(http.responseText);
+					}
+				}
+
+				http.send(param);
+
 }
--- src/script.js
+++ src/script.js
@@ -1,40 +1,81 @@
 			$('#arquivos').change(function(e){
-				var arq_img = [];
-				arq_img.push(e.files());
-				for(var i = 0; i < arq_img[0].length; i++){
-
-					new converter(event.files()[i], function(url){
-						var img = new Image(); 
-						img.style.width = '100%'; 
-						img.style.height = '100%';
-						img.src = url;
-						var ancora = document.createElement('a');
-                		ancora.href = url;
-                		ancora.target = '__blank';
-                		ancora.appendChild(img);
-                		$('.image .scroll').append(ancora);
+				var arqs = [];
+				arqs.push(e.files());
+				for(var i = 0; i < arqs[0].length; i++){
+					new converter([arqs[0][i], arqs[0][i].name, arqs[0][i].type], 
+						function(url, configs){	
+           			salvar([configs.name, configs.type, url], 
+           				function(r){
+           					console.log(r);
+           					var m = JSON.parse(r);
+           					if(m.msg == 'sucesso'){
+           						var img = new Image(); 
+								      img.style.width = '100%'; 
+								      img.style.height = '100%';
+								      img.src = m.src;
+								      var ancora = document.createElement('a');
+                			ancora.href = m.src;
+                			ancora.target = '__blank';
+                			ancora.appendChild(img);
+                			$('.image .scroll').append(ancora);
+           					}else{
+           						console.log('Erro');
+           					}
+            	});
 					});
-
 				}
-			});
+		});
 
-			$('#musicas').change(function(event){
-				var arq_musicas = [];
-				arq_musicas.push(event.files());
-				for(var i = 0; i < arq_musicas[0].length; i++){
+$('#musicas').change(function(event){
+  var arq_musicas = [];
+	arq_musicas.push(event.files());
 
-                new converter(event.files()[i], function(url){
-                			var a = document.createElement('a');
-                			var audio = new Audio();
-							audio.controls = 'controls';
-							var sour = document.createElement('source');
-							sour.src = url;
-							//sour.type = "audio/mpeg";
-							audio.append(sour);
-							a.href = url;
-							a.append(audio);
-							$('.audio .scroll').append(a);
-                });
+	for(var i = 0; i < arq_musicas[0].length; i++){
 
-				}
-			});
+     new converter([arq_musicas[0][i],arq_musicas[0][i].name,arq_musicas[0][i].type], 
+          function(url, config){
+             salvar([config.nome, config.type, url], 
+               function(retorno){
+                	var mss = JSON.parse(retorno);
+                	if(mss.msg != 'erro'){
+                	var a = document.createElement('a');
+                	var audio = new Audio();
+									audio.controls = 'controls';
+									var sour = document.createElement('source');
+									sour.src = mss.src;
+									audio.append(sour);
+									a.href = mss.src;
+									a.append(audio);
+									$('.audio .scroll').append(a);
+                }
+            });
+        });
+    }
+});
+
+$("#videos").change(function(ob){
+
+    var videos = [];
+    videos.push(ob.files());
+
+    for(var i = 0; i < videos[0].length; i++){
+
+      new converter([videos[0][i], videos[0][i].name, videos[0][i].type],
+        function(url, config){
+
+          var a = document.createElement('a'),
+              v = document.createElement('video');
+              v.autoplay = true;
+              v.controls = true;
+              var source = document.createElement('source');
+              source.src = url;
+              source.type = config.type;
+              a.target = '__blank';
+              a.href = url;
+              v.appendChild(source);
+              a.appendChild(v);
+              $('.video .scroll').append(a);
+
+        });
+    }
+});
